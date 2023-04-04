1c5906b3a6a9b8e4b182fa58469ac40f5a582ce5
Julie Moura <juliemouradev@gmail.com>
2023-04-04 16:11:45 -0300

    update: improvements

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,8 @@ export default function Home() {
       <div style={{ height: "100vh", display: "flex", alignItems: "center", justifyContent: "center", gap: "20px", paddingBottom: "40px" }}>
         <Link title="Tico e Teco" href="/ticoeteco" />
         <Link title="Palíndromo" href="/palindromo" />
+        <Link title="Link do Github" href="https://github.com/juliemoura/test-UI" />
       </div>
-      <Link title="Link do Github" href="https://github.com/juliemoura/test-UI" />
     </ThemeProvider>
   )
 }
